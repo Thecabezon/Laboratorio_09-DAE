ad937bddd60b59e29f49c98ad236e8e37a17434c
yudi-star <yudith.pacco@tecsup.edu.pe>
2025-05-13 11:20:02 -0500

    primer componente creado

---
+++ src/components/SerieComponent.jsx
@@ -0,0 +1,17 @@
+function SerieComponent() {
+    return (
+      <div className="card">
+        <img
+          className="card-img-top"
+          src="https://dummyimage.com/400x250/000/fff&text=Law+and+order"
+          alt="img" />
+        <div className="card-body">
+          <h5 className="card-title">Law & orden</h5>
+          <p className="card-text">Drama</p>
+          <button className="btn btn-primary">Description</button>
+        </div>
+      </div>
+    )
+  }
+  
+  export default SerieComponent
