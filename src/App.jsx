b4a4f64905f0924150b9b479319304645ca865ca
yudi-star <yudith.pacco@tecsup.edu.pe>
2025-05-13 11:31:53 -0500

    modificando App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SerieComponent from "./components/SerieComponent"
 
 function App() {
   return (
@@ -9,48 +6,32 @@ function App() {
       <h1 className="border-bottom pb-3 mb-3">Series</h1>
       <div className="row">
         <div className="col-md-4">
-          <div className="card">
-            <img 
-              className="card-img-top" 
-              src="https://dummyimage.com/400x250/000/fff&text=Friends" 
-              alt="img" />
-            <div className="card-body">
-              <h5 className="card-title">Friends</h5>
-              <p className="card-text">Comedy</p>
-              <button className="btn btn-primary">Description</button>
-            </div>
-          </div>
+          <SerieComponent
+            codigo="1"
+            nombre="Friends"
+            categoria="Comedy"
+            imagen="friends.png"
+          />
         </div>
         <div className="col-md-4">
-          <div className="card">
-            <img 
-              className="card-img-top" 
-              src="https://dummyimage.com/400x250/000/fff&text=Law+and+order" 
-              alt="img" />
-            <div className="card-body">
-              <h5 className="card-title">Law & orden</h5>
-              <p className="card-text">Drama</p>
-              <button className="btn btn-primary">Description</button>
-            </div>
-          </div>
+          <SerieComponent
+            codigo="2"
+            nombre="Law & Order"
+            categoria="Drama"
+            imagen="law-and-order.png"
+          />
         </div>
         <div className="col-md-4">
-          <div className="card">
-            <img 
-              className="card-img-top" 
-              src="https://dummyimage.com/400x250/000/fff&text=The+Big+Bang+Theory" 
-              alt="img" />
-            <div className="card-body">
-              <h5 className="card-title">The Big Bang Theory</h5>
-              <p className="card-text">Comedy</p>
-              <button className="btn btn-primary">Description</button>
-            </div>
-          </div>
+          <SerieComponent
+            codigo="3"
+            nombre="The Big Bang Theory"
+            categoria="Comedy"
+            imagen="the-big-bang-theory.png"
+          />
         </div>
       </div>
     </div>
   )
 }
-
 
 export default App
